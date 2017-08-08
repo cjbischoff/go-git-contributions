6d766401831b9f0d65046002a95ffc53cd2865fd
Christopher.Bischoff <christopher.bischoff@phishme.com>
2017-08-08 09:46:02 -0600

    create/start-application Completed scan library

--- scan.go
+++ scan.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"os/user"
+	"path/filepath"
 	"strings"
 )
 
@@ -40,4 +41,114 @@ func openFile(filePath string) *os.File {
 		}
 	}
 	return f
+}
+
+// parseFileLinesToSlice  given a file path string, gets the content
+// of each line and parses it to a slice of strings
+func parseFileLinesToSlice(filePath string) []string {
+	f := openFile(filePath)
+	defer f.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != null {
+		if err != io.EOF {
+			panic(err)
+		}
+	}
+
+	return lines
+
+}
+
+// sliceContains returns true if `slice` contains `value`
+func sliceContains(slice []string, value string) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+	return false
+
+}
+
+//joinSlices adds the element of the `new` slice
+// into the `existing` slice only if not already there
+func joinSlices(new []string, existing []string) []string {
+	for _, i := range new {
+		if !sliceContains(existing, i) {
+			existing = append(existing, i)
+		}
+	}
+	return existing
+}
+
+// dumpStringSliceToFile writes content to the file in path `filePath` (overwritting existing content)
+func dumpStringSliceToFile(repos []string, filePath string) {
+	content := strings.Join(repos, "\n")
+	ioutil.WriteFile(filePath, []byte(content), 0755)
+}
+
+// addNewSliceElementsToFile given a slice of strings reprensenting paths, stores them
+// to filesystem
+func addNewSliceElementsToFile(filePath string, newRepos []string) {
+	existingRepos := parseFileLinesToSlice(filePath)
+	repos := joinSlices(newRepos, existingRepos)
+	dumpStringSliceToFile(repos, filePath)
+}
+
+//recursiveScanFolder starets the recursive search of git repositorries
+// living in the `folder` structure
+func recursiveScaneFolder(folder string) []string {
+	return scanGitFolders(make([]string, 0), folder)
+}
+
+// scan scans a new folder for Git repositories
+func scan(folder string) {
+	fmt.Printf("Found folders:\n\n")
+	repositories := recursiveScanFolder(folder)
+	filePath := getDotFilePath()
+	addNewSliceElementsToFile(filePath, repositories)
+	fmt.Printf("\n\nSuccessfully added\n\n")
+}
+
+// scanGitFolders returns a list of subfolders of `folder` ending with `.git`.
+// Returns the base folder of the repo, the .git folder parent.
+// Recursively searches in the subfolders by passing an existing `folders` slice.
+func scanGitFolders(folders []string, folder string) []string {
+	// trim the last `/`
+	folder = strings.TrimSuffix(folder, "/")
+
+	f, err := os.Open(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var path string
+
+	for _, file := range files {
+		if file.IsDir() {
+			path = folder + "/" + file.Name()
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				fmt.Println(path)
+				folders = append(folders, path)
+				continue
+			}
+			if file.Name() == "vendor" || file.Name() == "node_modules" {
+				continue
+			}
+			folders = scanGitFolders(folders, path)
+		}
+	}
+
+	return folders
 }
