98e48669c39eba881b576c648d53495d2a9d4d90
Christopher.Bischoff <christopher.bischoff@phishme.com>
2017-08-07 19:29:09 -0600

    create/start-application
    - create simple skeleton for separation of concerns
    - started the scan lib
    - started the stats lib

---
+++ scan.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+	"strings"
+)
+
+// getDotFilePath return the dot file for the repos list.
+// Creates it and enclosing folder if it does exist
+func getDotFilePath() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dotFile := usr.HomeDir + "/.gogitlocalstats"
+
+	return dotFile
+}
+
+// openFile opens the file located at `filePath`. Creates it if not existing.
+func openFile(filePath string) *os.File {
+	f, err := os.OpenFile(filePath, os.O_APPEND|os.O_RDWR, 0755)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// file does not exist
+			_, err = os.Create(filePath)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			// other error
+			panic(err)
+		}
+	}
+	return f
+}
---
+++ stats.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"time"
+
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+)
+
+const outOfRange = 99999
+const daysInLastSixMonths = 183
+const weeksInLastSixMonth = 26
+
+type column []int
+
+// stats calculates and print the stats
+func stats(email string) {
+	commits := processRepositories(email)
+	printCommitsStats(commits)
+}
+
+//getBeginningOfDay given a time.Time calculates the start time of that day
+func getBeginningOfDay(t time.Time) time.Time {
+	year, month, day := t.Date()
+	startOfDay := time.Date(year, month, day, 0, 0, 0, 0, t.Location())
+	return startOfDay
+
+}
+
+// countDaySinceDate counts how many days passed since the pass `date`
+func countDaySinceDate(date time.Time) int {
+	days := 0
+	now := getBeginningOfDay(time.Now())
+	for date.Before(now) {
+		date = date.Add(time.Hour * 24)
+		days++
+		if days > daysInLastSixMonths {
+			return outOfRange
+		}
+	}
+	return days
+}
